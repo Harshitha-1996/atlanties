74e6930033f32497432f678495eb985f6cbe3b6e
Harshitha-1996 <harshithahm1996@gmail.com>
2024-10-18 16:07:04 +0530

    Update main.tf

--- first/main.tf
+++ first/main.tf
@@ -1,13 +1,4 @@
-terraform {
-  backend "s3" {
-  
-    bucket = "terraform-s3-bucket-new1996"
-    key="backend/terraform.tfstate"
-    region = "ap-south-1"
-    dynamodb_table = "backendtabel"
-    
-  }
-}
+
 
 resource "aws_instance" "first_ec2" {
     ami="ami-0522ab6e1ddcc7055"
