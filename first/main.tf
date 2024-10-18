68cf280ca5608aaa4373c66cf73c536f2ab76790
Harshitha-1996 <harshithahm1996@gmail.com>
2024-10-18 15:20:47 +0530

    Update main.tf

--- first/main.tf
+++ first/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "first_ec2" {
     ami="ami-0522ab6e1ddcc7055"
     instance_type="t2.micro"
     tags={
-        Name = "ec2_from_terra"
+        Name = "ec2_from_atlanties"
     }
     key_name = "jenkins_key_pair"
   
