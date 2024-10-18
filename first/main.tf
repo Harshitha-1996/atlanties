7def213615bf3efa8dc10cee9a2d9c186d7ddf98
Harshitha-1996 <harshithahm1996@gmail.com>
2024-10-18 16:23:48 +0530

    Update main.tf

--- first/main.tf
+++ first/main.tf
@@ -1,5 +1,4 @@
 
-
 resource "aws_instance" "first_ec2" {
     ami="ami-0522ab6e1ddcc7055"
     instance_type="t2.micro"
@@ -7,4 +6,19 @@ resource "aws_instance" "first_ec2" {
         Name = "ec-2"
     }
     key_name = "jenkins_key_pair"
+}
+
+resource "aws_s3_bucket" "existing_bucket" {
+  bucket = "terraform-s3-bucket-new1996"
+  # Other configurations...
+}
+
+resource "aws_dynamodb_table" "existing_table" {
+  name           = "backendtabel"
+  billing_mode   = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+  # Other configurations...
 }
